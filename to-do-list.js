6a95dfde36387d6b25d418b34413668a73ff4eb2
Aya Abdulsamie <ayaabdo729@gmail.com>
2021-02-05 12:12:41 +0200

    done el hamdulallah

--- to-do-list.js
+++ to-do-list.js
@@ -15,23 +15,40 @@ addButton.addEventListener("click",function(e){
 function getValuesFromLocalStorag()
 {
     let storedVal = localStorage.getItem("todos");
-    if(!storedVal) 
-    {
+    if(!storedVal) {
         todos.innerHTML = "";
-    }
-    else{
+
+    } else{
         todos.innerHTML = storedVal;
     }
+
     let comVal = localStorage.getItem("completed");
-    if(!comVal)
-    {
+    if(!comVal){
         completed.innerHTML = "";
-    }
-    else{
+
+    }else{
         completed.innerHTML = comVal;
     }
+    //console.log(todos.childNodes[0].id);
+    addActions();
 }
 
+function addActions()
+{
+    
+    for(let i = 0;i < todos.childNodes.length;++i){
+        let deleteButton = document.getElementById(`del${todos.childNodes[i].id}`);
+        let checkBox = document.getElementById(`check${todos.childNodes[i].id}`);
+        deleteButton.onclick = removeTask;
+        checkBox.onclick = changeStatus;
+    }
+    for(let i = 0;i < completed.childNodes.length;++i){
+        let deleteButton = document.getElementById(`del${completed.childNodes[i].id}`);
+        let checkBox = document.getElementById(`check${completed.childNodes[i].id}`);
+        deleteButton.onclick = removeTask;
+        checkBox.onclick = changeStatus;
+    }
+}
 
 function addNewTask(id)
 {
@@ -40,25 +57,22 @@ function addNewTask(id)
         li.setAttribute("class","todos")
 
         var checkBox = document.createElement('input');
-        checkBox.setAttribute("class","checkbox");
         checkBox.setAttribute("type","checkbox");
         checkBox.setAttribute("id",`check${id}`);
-        checkBox.setAttribute("name","check");
 
         var labelCheckBox = document.createElement('label');
         labelCheckBox.setAttribute("for",`check${id}`);
 
 
         var deleteButton = document.createElement('input');
-        deleteButton.setAttribute("class","del");
         deleteButton.setAttribute("type","submit");
         deleteButton.setAttribute("value"," ");
+        deleteButton.setAttribute("id",`del${id}`);
 
        deleteButton.onclick = removeTask;
         
        var text = document.createElement('span');
         text.innerHTML = `${input.value}`;
-        text.setAttribute("class","task");
         text.setAttribute("id",`span${id}`);
 
         checkBox.onclick = changeStatus;
@@ -92,24 +106,20 @@ function changeStatus()
     var listItem=this.parentNode;
     console.log(listItem.getAttribute('id') );
     var ul=listItem.parentNode;
-    //ul.removeChild(listItem);
 
     if(ul.getAttribute('id') === "toDoList"){
         var check = document.getElementById(`check${listItem.getAttribute('id')}`);
         check.setAttribute("checked","true");
         var span = document.getElementById(`span${listItem.getAttribute('id')}`);
         span.style.textDecoration='line-through';
-        //var li = document.getElementById(`${listItem.getAttribute('id')}`);
         listItem.setAttribute("class","completed");
         completed.appendChild(listItem);
     }
     else{
         var check = document.getElementById(`check${listItem.getAttribute('id')}`);
         check.setAttribute("checked","false");
-        console.log(listItem.getAttribute('id') );
         var span = document.getElementById(`span${listItem.getAttribute('id')}`);
         span.style.textDecoration='none';
-        //var li = document.getElementById(`${listItem.getAttribute('id')}`);
         listItem.setAttribute("class","todos");
         todos.appendChild(listItem);
     }
